7c4f534905fcdbe274645f898abf96792535a8b4
Sagar-Patil-C94 <sagarp@ivoyant.com>
2024-02-12 16:55:29 +0530

    @Scope Annotation For BeanPrototype

--- src/main/java/com/ivoyant/springandspringboot/ClassImpl.java
+++ src/main/java/com/ivoyant/springandspringboot/ClassImpl.java
@@ -2,9 +2,12 @@ package com.ivoyant.springandspringboot;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class ClassImpl {
     @Autowired
     @Qualifier("A")
--- src/main/java/com/ivoyant/springandspringboot/SpringAndSpringBootApplication.java
+++ src/main/java/com/ivoyant/springandspringboot/SpringAndSpringBootApplication.java
@@ -9,6 +9,9 @@ public class SpringAndSpringBootApplication {
     public static void main(String[] args) {
         ApplicationContext applicationContext = SpringApplication.run(SpringAndSpringBootApplication.class, args);
         ClassImpl classImpl = applicationContext.getBean(ClassImpl.class);
+        ClassImpl classImpl1 = applicationContext.getBean(ClassImpl.class);
+        System.out.println(classImpl);
+        System.out.println(classImpl1);
         System.out.println(classImpl.greeting());
     }
 
