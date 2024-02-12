aaca2f112c3f2621696f74e7b7f9583d597f2bf1
Sagar-Patil-C94 <sagarp@ivoyant.com>
2024-02-12 21:01:30 +0530

    CDI Framework of Dependency Injection

---
+++ src/main/java/com/ivoyant/springandspringboot/SpringCdiApplication.java
@@ -0,0 +1,20 @@
+package com.ivoyant.springandspringboot;
+
+import com.ivoyant.springandspringboot.beanlifecycle.ClassLife;
+import com.ivoyant.springandspringboot.cdi.SomeBusiness;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringCdiApplication {
+    private static Logger logger = LoggerFactory.getLogger(SpringCdiApplication.class);
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = SpringApplication.run(SpringCdiApplication.class, args);
+        SomeBusiness someBusiness = applicationContext.getBean(SomeBusiness.class);
+        logger.info("{}", someBusiness);
+        logger.info("{}", someBusiness.getDao());
+    }
+}
