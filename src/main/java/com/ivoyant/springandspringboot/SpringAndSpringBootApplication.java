b07d5897bebe028aa6711c3633fd7524aebb09f9
Sagar-Patil-C94 <sagarp@ivoyant.com>
2024-02-12 09:46:16 +0530

    Initial Commit

---
+++ src/main/java/com/ivoyant/springandspringboot/SpringAndSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.ivoyant.springandspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndSpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAndSpringBootApplication.class, args);
+    }
+
+}
